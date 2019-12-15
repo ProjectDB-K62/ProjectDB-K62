3b0a3a806da24d7c92ac6e61ce9cdb7ad02b6c8f
bichninh <58477752+bichninh@users.noreply.github.com>
2019-12-15 19:11:45 +0700

    CSDL

---
+++ webproduct .sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2019 at 11:48 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webproduct`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `gmail` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`, `gmail`) VALUES
+(32, 'Trần Ngọc Công', 'TranCong', 'c3725bde386e9fa2b4498699df403986', 'TranCong@gmail.com'),
+(33, 'Ninh Thị Bích', 'NinhBich', 'c3725bde386e9fa2b4498699df403986', 'NinhBich@gmail.com'),
+(35, 'Nguyễn Văn Công', 'NguyenCong', 'c3725bde386e9fa2b4498699df403986', 'NguyenCong@gmail.com'),
+(36, 'Dương Văn Đức', 'DuongDuc', 'c3725bde386e9fa2b4498699df403986', 'DuongDuc@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`brand_id`, `brand_name`) VALUES
+(1, 'ASUS'),
+(2, 'Apple'),
+(3, 'Pisen');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catalog`
+--
+
+CREATE TABLE `catalog` (
+  `cata_id` int(11) NOT NULL,
+  `cata_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `catalog`
+--
+
+INSERT INTO `catalog` (`cata_id`, `cata_name`) VALUES
+(3, 'Laptop'),
+(6, 'Phone'),
+(15, 'Accessories');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedback_id` int(11) NOT NULL,
+  `customer_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `feedback` text COLLATE utf8_unicode_ci NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedback_id`, `customer_name`, `customer_phone`, `customer_email`, `feedback`, `message`) VALUES
+(1, 'cong', '', 'abc', 'abc', 'abc');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(255) NOT NULL,
+  `customer_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp(3) NOT NULL DEFAULT current_timestamp(3) ON UPDATE current_timestamp(3)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`order_id`, `customer_name`, `customer_phone`, `customer_email`, `customer_address`, `created`) VALUES
+(3, 'cong', '0964337651', 'conghoa1cnh@gmail.com', 'abcd', '2019-12-15 09:16:52.660');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `detail_id` int(255) NOT NULL,
+  `order_id` int(255) NOT NULL,
+  `product_id` int(255) NOT NULL,
+  `qty` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`detail_id`, `order_id`, `product_id`, `qty`) VALUES
+(7, 3, 71, 3),
+(6, 3, 70, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(255) NOT NULL,
+  `catalog_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` decimal(15,0) NOT NULL,
+  `discount` decimal(15,0) NOT NULL,
+  `image_link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `image_list` text COLLATE utf8_unicode_ci NOT NULL,
+  `count_buy` int(11) NOT NULL DEFAULT 0,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `catalog_id`, `brand_id`, `name`, `price`, `discount`, `image_link`, `image_list`, `count_buy`, `description`, `amount`) VALUES
+(69, 3, 1, 'ASUS X555U', '600', '0', '1540398807-340-asus-x555u-21.jpg', '[\"1540398807-340-asus-x555u-22.jpg\"]', 0, '4G RAM, WINDOWS, 2.3Kg', 10),
+(70, 3, 2, 'Macbook pro 15 inch 2019', '1800', '200', '651656-meet-the-2019-apple-macbook-pro-15-inch.jpg', '[\"651656-meet-the-2019-apple-macbook-pro-15-inch1.jpg\",\"651656-meet-the-2019-apple-macbook-pro-15-inch2.jpg\",\"651656-meet-the-2019-apple-macbook-pro-15-inch3.jpg\"]', 3, '4G RAM, MacOS, 1.2Kg', 7),
+(71, 6, 2, 'Iphone X', '1300', '0', 'Puro-0-3-Nude-TPU-Case-for-iPhone-11-Pro-Max-Transparent-8033830281204-19092019-001-p.jpg', '[\"Puro-0-3-Nude-TPU-Case-for-iPhone-11-Pro-Max-Transparent-8033830281204-19092019-001-p1.jpg\",\"Puro-0-3-Nude-TPU-Case-for-iPhone-11-Pro-Max-Transparent-8033830281204-19092019-001-p2.jpg\",\"Puro-0-3-Nude-TPU-Case-for-iPhone-11-Pro-Max-Transparent-8033830281204-19092019-001-p3.jpg\"]', 3, '4G Ram, ios', 9),
+(72, 15, 3, 'Pin Pisen', '50', '0', 'download.jpg', '[\"download1.jpg\",\"download2.jpg\"]', 0, 'ừefrfr', 22222);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `catalog`
+--
+ALTER TABLE `catalog`
+  ADD PRIMARY KEY (`cata_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`detail_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `catalog`
+--
+ALTER TABLE `catalog`
+  MODIFY `cata_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `detail_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
